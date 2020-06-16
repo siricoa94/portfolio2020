b35c5a05eaf184a1aea23005e998514e874eeb65
unknown <12siricoa94@gmail.com>
2020-06-15 20:09:29 -0400

    added changes to index.css and added contact.html along with css

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ $("#aboutBtn").on("click", function (event) {
 $("#homeBtn").on("click", function (event) {
     location.href="index.html";
 });
+$("#contactBtn").on("click", function (event) {
+    location.href="contact.html";
+});
+$(".picture").on("click", function (event) {
+    let id = $(this).data("id");
+    switch(id){
+        case "funWithForums":
+            location.href = "https://funwithforums.herokuapp.com/";
+        break;
+        case "soundFound":
+            location.href = "https://foundsound.herokuapp.com/";
+        break;
+        case "ricoDepot":
+            location.href = "https://ricodepot.herokuapp.com/";
+        break;
+    };
+});
